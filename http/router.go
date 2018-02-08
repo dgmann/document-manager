9b1747310306bc8ba68c2226e25a24235c524859
David <support@davidgassmann.de>
2018-02-08 13:47:37 +0100

    Added tags endpoint

--- http/router.go
+++ http/router.go
@@ -20,5 +20,16 @@ func Run(a *shared.App) {
 
 	registerWebsocket(router)
 	registerRecords(router.Group("/records"))
+
+	router.GET("tags", func(context *gin.Context) {
+		tags, err := app.Tags.All()
+		if err != nil {
+			context.AbortWithError(500, err)
+			return
+		}
+
+		context.JSON(200, tags)
+	})
+
 	router.Run()
 }

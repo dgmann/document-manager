6f8a8507ddeb6807f689c05500c3eabb70b3f999
David <support@davidgassmann.de>
2018-04-03 11:16:53 +0200

    Cleaned up and refactored document edit dialog component

--- app/src/app/shared/document-edit-dialog/document-edit-dialog.component.ts
+++ app/src/app/shared/document-edit-dialog/document-edit-dialog.component.ts
@@ -1,12 +1,8 @@
-import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
 import {FormControl} from "@angular/forms";
-import {MAT_DIALOG_DATA, MatAutocompleteSelectedEvent, MatChipInputEvent, MatDialogRef} from "@angular/material";
-import {filter} from 'lodash-es';
-import {Observable} from "rxjs/Observable";
-import {map, startWith, switchMap, take} from "rxjs/operators";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {take} from "rxjs/operators";
 import {ReplaySubject} from "rxjs/ReplaySubject";
-import {Subject} from "rxjs/Subject";
 
 
 import {Record} from "../../store";
@@ -20,25 +16,10 @@ import {TagService} from "../tag-service";
   styleUrls: ['./document-edit-dialog.component.scss']
 })
 export class DocumentEditDialogComponent implements AfterViewInit, OnInit {
-  tagsFormControl = new FormControl();
-  categoryFormControl = new FormControl();
-
   @ViewChild('datepickertoogle', {read: ElementRef}) datepickerToggle;
-  tagInput = new Subject<string>();
-  categoryInput = new Subject<string>();
-
-  availableTags: Observable<string[]>;
-  filteredOptions: Observable<string[]>;
-  filteredCategories: Observable<string[]>;
-  categories: string[];
-
-  selectable: boolean = true;
-  removable: boolean = true;
-  addOnBlur: boolean = false;
-
-  separatorKeysCodes = [ENTER, COMMA];
   public record: Record;
   public tabIndex = new ReplaySubject<number>();
+  public categoryFormControl: FormControl;
 
   constructor(public dialogRef: MatDialogRef<DocumentEditDialogComponent>,
               @Inject(MAT_DIALOG_DATA) record: Record,
@@ -58,83 +39,25 @@ export class DocumentEditDialogComponent implements AfterViewInit, OnInit {
     else {
       this.tabIndex.next(0);
     }
-    this.availableTags = tagsService.get();
-    tagsService.getPrimaryTags().subscribe(cat => this.categories = cat);
+    this.categoryFormControl = new FormControl(this.record.category);
+    this.categoryFormControl.valueChanges.subscribe(val => this.record.category = val);
   }
 
   ngOnInit() {
-    this.filteredOptions = this.tagInput
-      .pipe(
-        startWith(''),
-        switchMap((val: string) => this.filter(val))
-      );
-    this.filteredCategories = this.categoryInput
-      .pipe(
-        map(val => filter(this.categories, cat => cat.startsWith(val)))
-      );
+
   }
 
   ngAfterViewInit(): void {
     this.datepickerToggle.nativeElement.querySelector('button').setAttribute('tabindex', '-1');
   }
 
-  add(event: MatChipInputEvent, autocomplete): void {
-    let input = event.input;
-    let value = event.value;
-
-    if (input) {
-      this.resetInputValue(input);
-    }
-    if (autocomplete.isOpen) {
-      return;
-    }
-
-    if ((value || '').trim()) {
-      this.record.tags.push(value.trim());
-    } else {
+  submit() {
+    if (this.valid()) {
       this.dialogRef.close(this.record);
     }
   }
 
-  addOption(event: MatAutocompleteSelectedEvent, tagInput): void {
-    let value = event.option.value;
-
-    if ((value || '').trim()) {
-      this.record.tags.push(value.trim());
-    }
-    this.resetInputValue(tagInput);
-  }
-
-  remove(tag: string): void {
-    const index = this.record.tags.indexOf(tag);
-    if (index > -1) {
-      this.record.tags.splice(index, 1);
-    }
-  }
-
-  inputChanged(event) {
-    let data = event.currentTarget.value;
-    if (data) {
-      this.tagInput.next(data);
-    }
-  }
-
-  categoryChanged(event) {
-    let data = event.currentTarget.value;
-    if (data) {
-      this.categoryInput.next(data);
-    }
-  }
-
-  filter(val: string) {
-    return this.availableTags.pipe(
-      map(tags => tags.filter(option => option.toLowerCase().indexOf(val.toLowerCase()) === 0)),
-      map(tags => tags.filter(tag => this.record.tags.findIndex(t => t.toLowerCase() == tag.toLowerCase()) < 0))
-    );
-  }
-
-  resetInputValue(tagInput) {
-    tagInput.value = "";
-    this.tagInput.next("");
+  valid() {
+    return this.record.date && this.record.patientId && this.record.category;
   }
 }

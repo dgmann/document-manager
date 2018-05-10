90b700185dffd55a431f8c4a432bced8944daaf9
David <support@davidgassmann.de>
2018-05-10 19:21:28 +0200

    Started refactoring notifications

--- app/src/app/shared/action-bar/action-bar.component.spec.ts
+++ app/src/app/shared/action-bar/action-bar.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ActionBarComponent } from './action-bar.component';
+import {ActionBarComponent} from './action-bar.component';
 
-describe('ActionBarComponent', () => {
+describe('EventSnackbarComponent', () => {
   let component: ActionBarComponent;
   let fixture: ComponentFixture<ActionBarComponent>;
 
--- app/src/app/shared/notification-service.ts
+++ app/src/app/shared/notification-service.ts
@@ -1,7 +1,10 @@
 import {Injectable} from "@angular/core";
 import {MatSnackBar} from "@angular/material";
+import {groupBy, map} from "lodash-es";
 import {Subject} from "rxjs";
+import {bufferTime, filter} from "rxjs/operators";
 import {Record} from "../store";
+import {EventSnackbarComponent} from "./event-snackbar/event-snackbar.component";
 
 @Injectable()
 export class NotificationService {
@@ -15,18 +18,63 @@ export class NotificationService {
   }
 
   logToConsole() {
-    this.events.subscribe(event => console.log(`${event.timestamp} ${event.message}: ${event.record && event.record.id}`))
+    this.events.subscribe(event => console.log(event.toString()))
   }
 
   logToSnackBar() {
-    this.events.subscribe(event => this.snackbar.open(event.message, '', {
-      duration: 3000
-    }));
+    this.events
+      .pipe(bufferTime(500), filter(events => events && events.length > 0))
+      .subscribe(events => {
+        const grouped = groupBy(events, 'payload.type');
+        const messages = map(grouped, (group, key) => {
+          const word = group.length == 1 ? "Befund" : "Befunde";
+          if (key == ActionType.UPDATED) {
+            return new GenericEvent({timestamp: new Date(), message: `${group.length} ${word} geändert.`});
+          } else if (key == ActionType.ADDED) {
+            return new GenericEvent({timestamp: new Date(), message: `${group.length} ${word} hinzugefügt.`});
+          } else if (key == ActionType.DELETED) {
+            return new GenericEvent({timestamp: new Date(), message: `${group.length} ${word} gelöscht.`});
+          } else {
+            return new GenericEvent({timestamp: new Date(), message: group.map(g => g.payload.message).join(' ,')});
+          }
+
+        });
+
+        this.snackbar.openFromComponent(EventSnackbarComponent, {
+          data: messages,
+          duration: 3000
+        })
+      });
   }
 }
 
 export interface NotificationEvent {
-  message: string;
-  timestamp: Date;
-  record?: Record;
+  payload: { timestamp: Date, message: string }
+
+  toString();
+}
+
+export class GenericEvent implements NotificationEvent {
+  constructor(public payload: { timestamp: Date, message: string }) {
+  }
+
+  public toString() {
+    return `${this.payload.timestamp}: ${this.payload.message}`;
+  }
+}
+
+export enum ActionType {
+  UPDATED = "updated",
+  ADDED = "added",
+  DELETED = "deleted",
+  NONE = "none"
+}
+
+export class RecordEvent implements NotificationEvent {
+  constructor(public payload: { type: ActionType, message: string, timestamp: Date, record: Record }) {
+  }
+
+  public toString() {
+    return `${this.payload.timestamp}, ${this.payload.record.id} ${this.payload.type}: ${this.payload.message}`;
+  }
 }

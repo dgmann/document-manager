97b63bad61799ab6dd401190db06006b07f13da3
David <support@davidgassmann.de>
2018-05-13 15:32:11 +0200

    PWA requirements added, closes #15

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { registerLocaleData } from '@angular/common';
+import {registerLocaleData} from '@angular/common';
 import localeDe from '@angular/common/locales/de';
-import { ErrorHandler, LOCALE_ID, NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatIconModule, MatSnackBarModule } from "@angular/material";
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgDragDropModule } from "ng-drag-drop";
-
-
-import { AppComponent } from './app.component';
-import { AppRoutesModule } from "./app.router";
-import { SharedModule } from "./shared";
-import { StoreModule } from "./store";
-import { DndModule } from "ng2-dnd";
-import { environment } from "../environments/environment";
+import {ErrorHandler, LOCALE_ID, NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule, MatIconModule, MatSnackBarModule} from "@angular/material";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker'
 import BugsnagErrorHandler from 'bugsnag-angular'
-import bugsnag from 'bugsnag-js'
+import bugsnag from 'bugsnag-js';
+import {NgDragDropModule} from "ng-drag-drop";
+import {DndModule} from "ng2-dnd";
+import {environment} from "../environments/environment";
+
+
+import {AppComponent} from './app.component';
+import {AppRoutesModule} from "./app.router";
+import {SharedModule} from "./shared";
+import {StoreModule} from "./store";
 
 
 const bugsnagClient = bugsnag(environment.bugsnagKey);
@@ -44,7 +45,8 @@ registerLocaleData(localeDe, 'de');
     StoreModule,
     NgDragDropModule.forRoot(),
     SharedModule.forRoot(),
-    DndModule.forRoot()
+    DndModule.forRoot(),
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'de-DE'},

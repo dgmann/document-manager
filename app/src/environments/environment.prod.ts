c8c1391961e4771fba47d9859e350249854ad5e2
David <support@davidgassmann.de>
2018-05-13 13:28:32 +0200

    - Finished dockerfile - Fixed drag-and-drop, closes #16

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "http://localhost:8080",
-  websocket: "ws://localhost:8080/notifications",
+  api: "http://192.168.178.38:8080",
+  websocket: "ws://192.168.178.38:8080/notifications",
   bugsnagKey: "44bd0ddb1369a54aa3cfa44bdf04f21c"
 };

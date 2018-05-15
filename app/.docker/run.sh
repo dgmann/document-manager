47d992a282b5b9eb449d93865e3513ede9e4f821
David <support@davidgassmann.de>
2018-05-15 10:00:31 +0200

    Allow to set configuration of APP via env variables, closes #20

---
+++ app/.docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+find /usr/share/nginx/html/main.*.js -type f -exec sed -i -e "s|{{!API_URL!}}|${API_URL}|" -e "s|{{!WS_URL!}}|${WS_URL}|" -e "s|{{!BUGSNAG_KEY!}}|${BUGSNAG_KEY}|" {} \;
+
+nginx -g 'daemon off;'

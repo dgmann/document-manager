cda6e1c9c2ead70d8a9c8f3acec397de24a7ee6e
David <support@davidgassmann.de>
2018-05-15 10:00:31 +0200

    Allow to set configuration of APP via env variables, closes #20

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "http://192.168.178.38:8080",
-  websocket: "ws://192.168.178.38:8080/notifications",
-  bugsnagKey: "44bd0ddb1369a54aa3cfa44bdf04f21c"
+  api: "{{!API_URL!}}",
+  websocket: "{{!WS_URL!}}",
+  bugsnagKey: "{{!BUGSNAG_KEY!}}"
 };

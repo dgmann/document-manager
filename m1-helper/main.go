5200738411e07513a56b7e7f78def59c26af30a8
David <support@davidgassmann.de>
2018-05-17 18:37:52 +0200

    Current Patient can be opened via hotkey, closes #22

--- m1-helper/main.go
+++ m1-helper/main.go
@@ -6,43 +6,67 @@ import (
 	"github.com/dgmann/document-manager/m1-helper/m1"
 	"io/ioutil"
 	"flag"
-	"github.com/gorilla/websocket"
+	"github.com/dgmann/document-manager/m1-helper/hotkey"
+	"os/exec"
+	"fmt"
 	"log"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
 func main() {
 	fileName := flag.String("f", "", "BDT file containing current patient")
+	serverUrl := flag.String("s", "http://localhost", "Document-Manager URL")
 	flag.Parse()
 	if *fileName == "" {
 		panic("no file provided")
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("BDT file path: " + *fileName))
-	})
-	http.HandleFunc("/control", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
+	registerHotKey(*fileName, *serverUrl)
+	startHttpServer(*fileName)
+}
+
+func toUtf8(iso88591Buf []byte) string {
+	buf := make([]rune, len(iso88591Buf))
+	for i, b := range iso88591Buf {
+		buf[i] = rune(b)
+	}
+	return string(buf)
+}
+
+func registerHotKey(fileName, serverUrl string) {
+	manager := hotkey.NewManager()
+	manager.Register(hotkey.Hotkey{1, hotkey.ModAlt + hotkey.ModCtrl, 'P'})
+	keyPresses := manager.Listen()
+	go func() {
+		for range keyPresses {
+			f, err := ioutil.ReadFile(fileName)
+			if err != nil {
+				println("error reading patient file")
+			}
+
+			patient, err := m1.Parse(toUtf8(f))
+
+			cmd := exec.Command(fmt.Sprintf("%s/patient/%s", serverUrl, patient.Id))
+			err = cmd.Run()
+
+			if err != nil {
+				fmt.Printf("an error occurred: %s\n", err)
+				log.Fatal(err)
+			}
 		}
-		conn.WriteJSON(struct {
-			command string
-		}{
-			command: "navigate",
-		})
+	}()
+}
+
+func startHttpServer(fileName string) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("BDT file path: " + fileName))
 	})
+
 	http.HandleFunc("/patient", func(w http.ResponseWriter, r *http.Request) {
 		j := json.NewEncoder(w)
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json;charset=utf-8")
 
-		f, err := ioutil.ReadFile(*fileName)
+		f, err := ioutil.ReadFile(fileName)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			j.Encode(err)
@@ -56,12 +80,4 @@ func main() {
 		j.Encode(patient)
 	})
 	http.ListenAndServe(":3000", nil)
-}
-
-func toUtf8(iso8859_1_buf []byte) string {
-	buf := make([]rune, len(iso8859_1_buf))
-	for i, b := range iso8859_1_buf {
-		buf[i] = rune(b)
-	}
-	return string(buf)
 }

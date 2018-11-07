ff6335f457b0e1bfef6ce70f976d37d9915f71eb
David <support@davidgassmann.de>
2018-11-07 15:15:28 +0100

    Fix Document Edit Dialog.

--- app/src/app/core/tag-service.ts
+++ app/src/app/core/tag-service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class TagService {
+  public tags: Observable<string[]>;
+
   constructor(private http: HttpClient) {
+    this.tags = new BehaviorSubject<string[]>([]);
   }
 
-  public get() {
-    return this.http.get<string[]>(environment.api + "/tags");
+  public load() {
+    this.http.get<string[]>(environment.api + "/tags").subscribe(tags => (this.tags as BehaviorSubject<string[]>).next(tags));
   }
 
   public getByPatientId(id: string) {

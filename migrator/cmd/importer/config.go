a7296a45996ecec7453cef1ffcdc77dab3998414
David <support@davidgassmann.de>
2018-11-17 13:09:49 +0100

    Write records which were not imported to a special file.

--- migrator/cmd/importer/config.go
+++ migrator/cmd/importer/config.go
@@ -3,22 +3,22 @@ package main
 import "github.com/namsral/flag"
 
 type Config struct {
-	ApiURL    string
-	InputFile string
-	ErrorFile string
+	ApiURL        string
+	InputFile     string
+	DataDirectory string
 }
 
 func NewConfig() Config {
-	var apiURL, inputFile, errorFile string
+	var apiURL, inputFile, dataDir string
 
 	flag.StringVar(&inputFile, "input", "", "File containing file paths to import")
-	flag.StringVar(&errorFile, "error", "/data/errors.txt", "Write unsuccessfully imported PDFs to")
+	flag.StringVar(&dataDir, "data_dir", "/data", "Directory to store data")
 	flag.StringVar(&apiURL, "api_url", "http://localhost", "The URL of the API")
 	flag.Parse()
 
 	return Config{
-		ApiURL:    apiURL,
-		InputFile: inputFile,
-		ErrorFile: errorFile,
+		ApiURL:        apiURL,
+		InputFile:     inputFile,
+		DataDirectory: dataDir,
 	}
 }

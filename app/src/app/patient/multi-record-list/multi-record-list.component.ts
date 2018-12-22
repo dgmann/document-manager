9ee6eb61e1a1e1d85c38dfcf3015bdf1642eba7e
David <support@davidgassmann.de>
2018-12-22 15:56:57 +0100

    Add print stypesheet for patient view which allows to print the selected records

--- app/src/app/patient/multi-record-list/multi-record-list.component.ts
+++ app/src/app/patient/multi-record-list/multi-record-list.component.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { combineLatest, Observable } from "rxjs";
-import { Category } from "../../core";
-import { Record } from "../../core/store";
-import { distinctUntilChanged, filter, map, take } from "rxjs/operators";
-import { findIndex, groupBy, sortBy } from 'lodash-es'
-import { DocumentEditDialogService, EditResult, MessageBoxService } from "../../shared";
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {findIndex, groupBy, sortBy} from 'lodash-es'
+import {combineLatest, Observable} from "rxjs";
+import {distinctUntilChanged, filter, map, take, withLatestFrom} from "rxjs/operators";
+import {Category} from "../../core";
+import {Record} from "../../core/store";
+import {DocumentEditDialogService, EditResult, MessageBoxService} from "../../shared";
 
 @Component({
   selector: 'app-multi-record-list',
@@ -15,25 +15,38 @@ import { DocumentEditDialogService, EditResult, MessageBoxService } from "../../
 export class MultiRecordListComponent implements OnInit {
   @Input() records: Observable<Record[]>;
   @Input() selectedCategory: Observable<string>;
-  @Input() categories: { [id: string]: Category };
+  categoryMap: { [id: string]: Category } = {};
   @Output() clickRecord = new EventEmitter<string>();
   @Output() selectedCategoryChange = new EventEmitter<string>();
   @Output() updateRecord = new EventEmitter<EditResult>();
   @Output() deleteRecord = new EventEmitter<Record>();
   @Output() openInEditor = new EventEmitter<Record>();
-
+  allRecords: Observable<Record[]>;
   groupedRecords: Observable<{ category: string, records: Record[] }[]>;
+
+  @Input() set categories(value: { [id: string]: Category }) {
+    this.categoryMap = {
+      ...value,
+      all: {id: "all", name: "Alle"}
+    };
+  }
   selectedIndex: Observable<number>;
 
   constructor(private dialog: DocumentEditDialogService, private messageBox: MessageBoxService) {
   }
 
   ngOnInit() {
+    this.allRecords = this.records.pipe(
+      map(records => sortBy(records, ['category', 'date']))
+    );
+
     this.groupedRecords = this.records.pipe(
       map(records => groupBy(records, 'category')),
       map(grouped => Object.entries(grouped)
-        .map(entry => ({category: entry[0], records: entry[1]}))),
-      map(grouped => sortBy(grouped, ['category']))
+        .map(entry => ({category: entry[0], records: sortBy(entry[1], ['date'])}))),
+      map(grouped => sortBy(grouped, ['category'])),
+      withLatestFrom(this.records),
+      map(([grouped, records]) => [...grouped, {category: 'all', records: sortBy(records, ['date'])}])
     );
     this.selectedIndex = combineLatest(this.groupedRecords, this.selectedCategory).pipe(
       map(([categories, selectedCategory]) => findIndex(categories, {category: selectedCategory})),

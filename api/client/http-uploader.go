dd6c0903dd1e633786461ddd724bdb799d5dc292
David Gassmann <david.gassmann@gmail.com>
2019-06-16 17:53:58 +0200

    Remove api-client package

--- api/client/http-uploader.go
+++ api/client/http-uploader.go
@@ -21,7 +21,7 @@ func NewHttpUploader(url string) *HttpUploader {
 
 func (u *HttpUploader) Upload(create *NewRecord) error {
 	params := createParamMap(create)
-	req, err := newfileUploadRequest(u.url+"/records", params, File)
+	req, err := newfileUploadRequest(u.url+"/records", params, create.File)
 	if err != nil {
 		return err
 	}

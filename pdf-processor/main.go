24594e1e60e2e5d91203d6a81dc573c20accd07e
David Gassmann <david.gassmann@gmail.com>
2019-10-04 13:48:59 +0200

    Reenable pdf services

--- pdf-processor/main.go
+++ pdf-processor/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/dgmann/document-manager/pdf-processor/gopdf"
+	"github.com/dgmann/document-manager/pdf-processor/imagick"
 	"github.com/dgmann/document-manager/pdf-processor/pkg/processor"
+	"github.com/dgmann/document-manager/pdf-processor/poppler"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"net"
 )
 
 func main() {
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	rotator := imagick.NewProcessor()
+	converter := poppler.NewProcessor()
+
 	creator := gopdf.NewPdfCreator()
 	port := 9000
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -17,7 +25,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(1024*1024*300), grpc.MaxSendMsgSize(1024*1024*300))
-	processor.RegisterPdfProcessorServer(grpcServer, NewGRPCServer(nil, nil, creator))
+	processor.RegisterPdfProcessorServer(grpcServer, NewGRPCServer(converter, rotator, creator))
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Error(err)
 	}

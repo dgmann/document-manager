a023dddea829f3d71d25f2e29214e36be4385cd6
David <support@davidgassmann.de>
2019-12-22 21:20:22 +0100

    Debug output 4

--- api/storage/filesystem/storage.go
+++ api/storage/filesystem/storage.go
@@ -113,6 +113,7 @@ func (f *DiskStorage) ForEach(keyed storage.Keyed, forEachFn ForEachFunc) error 
 			logrus.Infof("Filename: %s", info.Name())
 			ext := filepath.Ext(info.Name())
 			abs := strings.Trim(currentPath, ext)
+			logrus.Infof("Ext: %v, CurrentPath: %v, Info: %v", ext, currentPath, info)
 			rel, err := filepath.Rel(f.Root, abs)
 			if err != nil {
 				logrus.WithError(err).Error(err.Error())

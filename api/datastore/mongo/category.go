83a56fa7f72d813647ecdb6915f130679003ec3d
David Gaßmann <david.gassmann@gmail.com>
2020-11-01 11:02:40 +0100

    Fixes category loading

--- api/datastore/mongo/category.go
+++ api/datastore/mongo/category.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+
 	"github.com/dgmann/document-manager/api/datastore"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -29,7 +30,7 @@ func (c *CategoryService) All(ctx context.Context) ([]datastore.Category, error)
 	if err != nil {
 		return nil, fmt.Errorf("while loading categories: %w", err)
 	}
-	err = cursor.Close(ctx)
+	defer cursor.Close(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("while closing cursor: %w", err)
 	}

31b1efb1d7b23b35f579cd31d7ce3ae54ed86dbd
David Gaßmann <david.gassmann@gmail.com>
2020-12-25 13:43:54 +0100

    Fixes bdt parser to ignore field length

--- m1-helper/bdt/parser.go
+++ m1-helper/bdt/parser.go
@@ -2,12 +2,18 @@ package bdt
 
 import (
 	"bufio"
-	"golang.org/x/text/encoding/charmap"
 	"io"
 	"strings"
 	"time"
+
+	"golang.org/x/text/encoding/charmap"
 )
 
+// Parse BDT file
+// Format:
+// - first three numbers define the field length, i.e 9 + length of value
+// - next 4 numbers defining the field id
+// - then the actual value (length as defined above)
 func Parse(data io.Reader) (*Patient, error) {
 	results := make(map[string]string)
 
@@ -19,20 +25,20 @@ func Parse(data io.Reader) (*Patient, error) {
 		if len(r) < 8 {
 			continue
 		}
-		key, value := string(r[:7]), string(r[7:])
+		key, value := string(r[2:7]), string(r[7:])
 		results[key] = value
 	}
 
 	patient := Patient{
-		Id:          results["0103000"],
-		FirstName:   results["0173102"],
-		LastName:    results["0163101"],
-		BirthDate:   toBirthDate(results["0173103"]),
-		PhoneNumber: results["0233626"],
+		Id:          results["3000"],
+		FirstName:   results["3102"],
+		LastName:    results["3101"],
+		BirthDate:   toBirthDate(results["3103"]),
+		PhoneNumber: results["3626"],
 		Address: Address{
-			Street: results["0233107"],
-			Zip:    results["0143112"],
-			City:   results["0193113"],
+			Street: results["3107"],
+			Zip:    results["3112"],
+			City:   results["3113"],
 		},
 	}
 	return &patient, nil

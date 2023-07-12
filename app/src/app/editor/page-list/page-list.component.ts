e68fff5525775f0e1cce76f46e8ab89e37607d31
dgmann <dgmann@users.noreply.github.com>
2023-07-12 09:56:02 +0200

    fix(editor): correct reordering

--- app/src/app/editor/page-list/page-list.component.ts
+++ app/src/app/editor/page-list/page-list.component.ts
@@ -1,19 +1,106 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import {PageUpdate} from '@app/core/records';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDragEnter, CdkDropList, DragRef, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
   styleUrls: ['./page-list.component.scss']
 })
-export class PageListComponent implements OnInit {
+export class PageListComponent implements AfterViewInit {
   @Input() pages: PageUpdate[];
+  @ViewChild(CdkDropList) placeholder: CdkDropList;
 
-  constructor() {
+  private target: CdkDropList = null;
+  private targetIndex: number;
+  private source: CdkDropList = null;
+  private sourceIndex: number;
+  private dragRef: DragRef = null;
+
+  boxWidth = '400px';
+
+  ngAfterViewInit() {
+    const placeholderElement = this.placeholder.element.nativeElement;
+
+    placeholderElement.style.display = 'none';
+    placeholderElement.parentNode.removeChild(placeholderElement);
   }
 
-  ngOnInit() {
+  onDropListDropped() {
+    if (!this.target) {
+      return;
+    }
+
+    const placeholderElement: HTMLElement =
+      this.placeholder.element.nativeElement;
+    const placeholderParentElement: HTMLElement =
+      placeholderElement.parentElement;
+
+    placeholderElement.style.display = 'none';
+
+    placeholderParentElement.removeChild(placeholderElement);
+    placeholderParentElement.appendChild(placeholderElement);
+    placeholderParentElement.insertBefore(
+      this.source.element.nativeElement,
+      placeholderParentElement.children[this.sourceIndex]
+    );
+
+    if (this.placeholder._dropListRef.isDragging()) {
+      this.placeholder._dropListRef.exit(this.dragRef);
+    }
+
+    this.target = null;
+    this.source = null;
+    this.dragRef = null;
+
+    if (this.sourceIndex !== this.targetIndex) {
+      moveItemInArray(this.pages, this.sourceIndex, this.targetIndex);
+    }
+  }
+
+  onDropListEntered({ item, container }: CdkDragEnter) {
+    if (container == this.placeholder) {
+      return;
+    }
+
+    const placeholderElement: HTMLElement =
+      this.placeholder.element.nativeElement;
+    const sourceElement: HTMLElement = item.dropContainer.element.nativeElement;
+    const dropElement: HTMLElement = container.element.nativeElement;
+    const dragIndex: number = Array.prototype.indexOf.call(
+      dropElement.parentElement.children,
+      this.source ? placeholderElement : sourceElement
+    );
+    const dropIndex: number = Array.prototype.indexOf.call(
+      dropElement.parentElement.children,
+      dropElement
+    );
+
+    if (!this.source) {
+      this.sourceIndex = dragIndex;
+      this.source = item.dropContainer;
+
+      placeholderElement.style.width = this.boxWidth + 'px';
+
+      sourceElement.parentElement.removeChild(sourceElement);
+    }
+
+    this.targetIndex = dropIndex;
+    this.target = container;
+    this.dragRef = item._dragRef;
+
+    placeholderElement.style.display = '';
+
+    dropElement.parentElement.insertBefore(
+      placeholderElement,
+      dropIndex > dragIndex ? dropElement.nextSibling : dropElement
+    );
+
+    this.placeholder._dropListRef.enter(
+      item._dragRef,
+      item.element.nativeElement.offsetLeft,
+      item.element.nativeElement.offsetTop
+    );
   }
 
   drop(event: CdkDragDrop<string[]>) {

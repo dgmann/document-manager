a3ac75427b83a8e942a0d26f37eea2b288e528f0
dgmann <dgmann@users.noreply.github.com>
2024-10-06 19:20:46 +0000

    feat(cli): add page check
    Signed-off-by: dgmann <dgmann@users.noreply.github.com>

--- internal/cli/check.go
+++ internal/cli/check.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"sort"
 	"sync"
 	"text/tabwriter"
 
 	"github.com/dgmann/document-manager/pkg/api"
+	"github.com/dgmann/document-manager/pkg/log"
 	"github.com/schollz/progressbar/v3"
 )
 
@@ -24,6 +26,8 @@ func (r *check) Execute(args []string) error {
 	switch args[0] {
 	case "patients":
 		return r.patients(subArgs)
+	case "pages":
+		return r.pages(subArgs)
 	default:
 		l.Fatalf("error: unknown command - %q\n", args[0])
 		return ErrUnknownCommand
@@ -31,8 +35,6 @@ func (r *check) Execute(args []string) error {
 }
 
 func (r *check) patients(args []string) error {
-	fs := flag.NewFlagSet("check", flag.ContinueOnError)
-	parallel := fs.Int("p", 4, "number of parallel workers")
 	slog.Info("fetching list of all patients. This may take some time...")
 	patients, err := dmClient.Patients.All()
 	if err != nil {
@@ -43,6 +45,37 @@ func (r *check) patients(args []string) error {
 		patientMap[patient.Id] = patient
 	}
 
+	slog.Info("fetching list of all records. This may take some time...")
+	records, err := dmClient.Records.List()
+	if err != nil {
+		return err
+	}
+	slog.Info("record list fetched", "count", len(records))
+	slog.Info("checking patients")
+
+	var unknownPatientRecords []api.Record
+	for _, record := range records {
+		if _, ok := patientMap[*record.PatientId]; !ok {
+			unknownPatientRecords = append(unknownPatientRecords, record)
+		}
+	}
+
+	sort.Slice(unknownPatientRecords, func(i, j int) bool {
+		return *unknownPatientRecords[i].PatientId < *unknownPatientRecords[j].PatientId
+	})
+
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+	defer w.Flush()
+	fmt.Fprintln(w, "Patient\tRecord\t")
+	for _, record := range unknownPatientRecords {
+		fmt.Fprintf(w, "%s\t%s\t\n", *record.PatientId, record.Id)
+	}
+	return nil
+}
+
+func (r *check) pages(args []string) error {
+	fs := flag.NewFlagSet("check-pages", flag.ContinueOnError)
+	parallel := fs.Int("p", 4, "number of parallel workers")
 	slog.Info("fetching list of all records. This may take some time...")
 	records, err := dmClient.Records.List()
 	if err != nil {
@@ -60,7 +93,7 @@ func (r *check) patients(args []string) error {
 	)
 
 	recordChan := make(chan api.Record)
-	unknownPatientRecordsChan := make(chan api.Record)
+	invalidPagesChan := make(chan api.Record)
 
 	var wg sync.WaitGroup
 	for range *parallel {
@@ -68,8 +101,19 @@ func (r *check) patients(args []string) error {
 		go func() {
 			defer wg.Done()
 			for record := range recordChan {
-				if _, ok := patientMap[*record.PatientId]; !ok {
-					unknownPatientRecordsChan <- record
+				for _, page := range record.Pages {
+					var resp *http.Response
+					for {
+						resp, err = http.Get(page.Url)
+						if err == nil {
+							break
+						}
+						slog.Debug("error fetching page", "recordId", record.Id, log.ErrAttr(err))
+					}
+					if resp.StatusCode != 200 {
+						invalidPagesChan <- record
+						break
+					}
 				}
 				bar.Add(1)
 			}
@@ -82,22 +126,15 @@ func (r *check) patients(args []string) error {
 		}
 		close(recordChan)
 		wg.Wait()
-		close(unknownPatientRecordsChan)
+		close(invalidPagesChan)
 	}()
-
-	var unknownPatientRecords []api.Record
-	for record := range unknownPatientRecordsChan {
-		unknownPatientRecords = append(unknownPatientRecords, record)
-	}
-	sort.Slice(unknownPatientRecords, func(i, j int) bool {
-		return *unknownPatientRecords[i].PatientId < *unknownPatientRecords[j].PatientId
-	})
 
 	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
 	defer w.Flush()
 	fmt.Fprintln(w, "Patient\tRecord\t")
-	for _, record := range unknownPatientRecords {
+	for record := range invalidPagesChan {
 		fmt.Fprintf(w, "%s\t%s\t\n", *record.PatientId, record.Id)
 	}
+
 	return nil
 }
